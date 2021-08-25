1a16f1f88838b01b2f20c132765fea225f6d229c
Clemens Werther <clemens.werther@gmail.com>
2021-08-25 12:46:15 +0200

    Improve dithering performance
    PIL can't dither to 4-bit grayscale. But since palette images can be
    dithered by PIL, using a 4-bit grayscale palette does the grayscale + dither
    conversion in one pass.

--- imageprepare.py
+++ imageprepare.py
@@ -4,6 +4,12 @@ from PIL import Image
 import math
 import zlib
 
+def grayscale_palette():
+    palette = []
+    for i in range(0, 255, 16):
+        palette += [i, i, i]
+    return palette * 16
+
 def calculate_crop_area(size_in, size_request):
     wi, hi = size_in
     w, h = size_request
@@ -23,38 +29,6 @@ def calculate_crop_area(size_in, size_request):
         y = (hi-ho)/2
     return (x, y, x+wo, y+ho)
 
-def dither(img):
-    px = img.load()
-    # TODO optimize. no full array needed
-    arr = [None]*(img.height * img.width)
-
-    for _y in range(img.height):
-        y = img.width * _y
-        for x in range(img.width):
-            arr[x+y] = px[x,_y]
-
-    for _y in range(img.height):
-        y = img.width * _y
-        for x in range(img.width):
-            oldpixel = arr[x + y]
-            newpixel = sorted((0, round(oldpixel / 16) * 16, 255))[1]
-            arr[x + y] = newpixel
-
-            quant_error = oldpixel - newpixel
-            if (x + 1 < img.width):
-                arr[x + 1 + y] += quant_error * 7 / 16
-                if _y + 1 < img.height:
-                  arr[x + 1 + y + img.width] += quant_error * 1 / 16
-
-            if _y + 1 < img.height:
-                arr[x + y + img.width] += quant_error * 5 / 16
-                if x > 0:
-                    arr[x - 1 + y + img.width] += quant_error * 3 / 16
-
-    for i in range(len(arr)):
-        px[i%img.width, i/img.width] = arr[i]
-    return img
-
 def to_raw(image):
     px = image.load()
     width, height = image.size
@@ -65,9 +39,9 @@ def to_raw(image):
     for y in range(height):
         for x in range(width):
             if i:
-                val = px[x, y] // 16
+                val = px[x, y]
             else:
-                val += px[x, y] // 16 << 4
+                val += px[x, y] << 4
                 buf.append(val)
             i = not i
     return buf
@@ -89,13 +63,19 @@ def prepare(filepath, size):
 
     if preprocessing_needed(im, size):
         print(f'Preprocessing needed ...')
+
         # crop & resize
         crop = calculate_crop_area(im.size, size)
         im = im.resize(size, resample=Image.LANCZOS, box=crop)
-        # grayscale
-        im = im.convert(mode='L')
-        # dither
-        im = dither(im)
+
+        # remove alpha channel to enable conversion to palette
+        im = im.convert('RGB')
+
+        # 4-bit grayscale & dither
+        palette = Image.new('P', (1, 1))
+        palette.putpalette(grayscale_palette())
+
+        im = im.quantize(palette=palette, dither=Image.FLOYDSTEINBERG)
 
     # show image for debugging purposes
     # im.show()
