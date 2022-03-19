eea7f39e547349325c67d7ac5a1f74afda1bb6e6
Clemens Werther <clemens.werther@gmail.com>
2022-03-20 00:27:25 +0100

    Add VLC cover art server example

---
+++ examples/vlc_cover_server/vlc_cover_gen.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import requests
+
+from matplotlib import font_manager
+from PIL import Image, ImageDraw, ImageFont
+
+
+def vlc_cover_gen(url, password):
+    im = Image.new('L', (1000, 1333))
+    draw = ImageDraw.Draw(im)
+
+    try:
+        r = requests.get(f'http://{url}/requests/status.json', auth=('', password))
+        if r.status_code != 200:
+            print(f'Unsuccessful: {r.status_code}')
+            return (im, -1)
+        r.encoding = 'utf-8'
+        data = r.json()
+    except Exception as e:
+        print('Error:', e)
+        return (im, -1)
+
+
+    current_pos = int(data['time'])
+    length = int(data['length'])
+    remaining = length - current_pos
+
+    try:
+        artist = data['information']['category']['meta']['artist']
+        title = data['information']['category']['meta']['title']
+        artwork = data['information']['category']['meta']['artwork_url']
+        artwork = artwork.replace('file://', '')
+        artwork = requests.compat.unquote(artwork)
+    except Exception as e:
+        print('Error:', e)
+        return (im, remaining)
+    print(f'Artist: {artist}, title: {title}, remaining time: {remaining}')
+
+    cover = Image.open(artwork)
+    cover = cover.resize((1000, 1000))
+    im.paste(cover, (0, 333))
+
+    font = font_manager.FontProperties(family='sans-serif', weight='bold')
+    fontfile = font_manager.findfont(font)
+    font = ImageFont.truetype(fontfile, 48)
+
+    draw.multiline_text((500,111), artist, font=font, fill=255, anchor='mm')
+    draw.multiline_text((500,222), title, font=font, fill=255, anchor='mm')
+
+    im = im.rotate(90, expand=True)
+    return (im, remaining)
+
+
+if __name__ == '__main__':
+    im, remaining = vlc_cover_gen('127.0.0.1:8080', ' ')
+    print(f'Remaining secs: {remaining}')
+    im.show()
---
+++ examples/vlc_cover_server/vlc_cover_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append("../../")
+
+import argparse
+from http.server import ThreadingHTTPServer
+
+from lib.imageserver import ImageRequestHandler
+from lib.imageprepare import prepare_image
+
+from vlc_cover_gen import vlc_cover_gen
+
+
+ARTFRAME_LIFECYCLE = 6  # the ArtFrame takes 2 secs to boot, and approximately another 4 to connect to WiFi and download an image
+
+def vlc_cover_response(vlcurl, password, config):
+    im, remaining = vlc_cover_gen(vlcurl, password)
+    out = prepare_image(im, config)
+
+    headers = [('Cache-Control', f'max-age={remaining - ARTFRAME_LIFECYCLE}')]
+    return (200, headers, out)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ArtFrame VLC cover art server.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--port', '-p', help='Port that the server will listen on', required=False, type=int, default=8090)
+    parser.add_argument('--vlcurl', '-u', help='Url of VLC\'s web interface', required=False, default='127.0.0.1:8080')
+    parser.add_argument('--password', '-q', help='Password for VLC\'s web interface', required=True)
+
+    args = parser.parse_args()
+    port = args.port
+    vlcurl = args.vlcurl
+    password = args.password
+    ImageRequestHandler.method = staticmethod(lambda config: vlc_cover_response(vlcurl, password, config))
+    print(f'Listening on port {port}')
+
+    httpd = ThreadingHTTPServer(('', port), ImageRequestHandler)
+    httpd.serve_forever()
