e6568004f6addf5fd289b9a913046df4f0d2da44
Clemens Werther <clemens.werther@gmail.com>
2022-03-20 01:05:15 +0100

    Allow execution of the examples with arbitrary working directories

--- examples/image_server/image_select.py
+++ examples/image_server/image_select.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append("../../")
-
 import os
 import random
 from pathlib import Path
--- examples/vlc_cover_server/vlc_cover_server.py
+++ examples/vlc_cover_server/vlc_cover_server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append("../../")
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
 
 import argparse
 from http.server import ThreadingHTTPServer
