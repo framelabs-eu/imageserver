b4df34322ea48ba36d418127e71f56616a400ce5
Simone Rescio <simone.rescio@gmail.com>
2023-01-17 18:36:13 +0100

    fix secret JSON load, add autocontrast in config

--- examples/gsheet_switch/gsheet_config-sample.py
+++ examples/gsheet_switch/gsheet_config-sample.py
@@ -1,3 +1,4 @@
 sheetId = 'yourGoogleSheetIDwhereYouStoredTheDirNames'
 sheetTab = 0 #integer, represents the pages in the file
 sheetCell = 'A1' #the COL/ROW index
+autocontrast = 0
--- examples/gsheet_switch/gsheet_select.py
+++ examples/gsheet_switch/gsheet_select.py
@@ -1,12 +1,14 @@
 import os
 from gsheets import Sheets
-from gsheet_config import sheetId, sheetTab, sheetCell
+from gsheet_config import sheetId, sheetTab, sheetCell, autocontrast
 
 from examples.image_server.image_select import random_file_response
 
 def gsheet_file_response(serve_path, config):
-    config.autocontrast = 3
-    sheets = Sheets.from_files('./client_secret.json')
+    config.autocontrast = autocontrast
+    currentFilePath = os.path.dirname(os.path.realpath(__file__))
+    clientSecretPath = os.path.join(currentFilePath, 'client_secret.json')
+    sheets = Sheets.from_files(clientSecretPath)
     gsheetData = sheets[sheetId]
     selectedDir = gsheetData[sheetTab][sheetCell]
     newPath = os.path.join(serve_path, selectedDir)
