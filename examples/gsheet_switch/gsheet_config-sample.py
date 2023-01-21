776cb4fe314e1d620e8d145cb10d1645a213067c
Simone Rescio <simone.rescio@gmail.com>
2023-01-21 19:14:14 +0100

    change camelCase 2 snake_case bc python is a snake

--- examples/gsheet_switch/gsheet_config-sample.py
+++ examples/gsheet_switch/gsheet_config-sample.py
@@ -1,4 +1,4 @@
-sheetId = 'yourGoogleSheetIDwhereYouStoredTheDirNames'
-sheetTab = 0 #integer, represents the pages in the file
-sheetCell = 'A1' #the COL/ROW index
-autocontrast = 0
+sheet_id = 'yourGoogleSheetIDwhereYouStoredTheDirNames'
+sheet_tab = 0 # integer, represents the pages in the file
+sheet_cell = 'A1' # the COL/ROW index
+autocontrast = 0 # how much to increase base image contrast
--- examples/gsheet_switch/gsheet_select.py
+++ examples/gsheet_switch/gsheet_select.py
@@ -1,16 +1,16 @@
 import os
 from gsheets import Sheets
-from gsheet_config import sheetId, sheetTab, sheetCell, autocontrast
+from gsheet_config import sheet_id, sheet_tab, sheet_cell, autocontrast
 
 from examples.image_server.image_select import random_file_response
 
 def gsheet_file_response(serve_path, config):
     config.autocontrast = autocontrast
-    currentFilePath = os.path.dirname(os.path.realpath(__file__))
-    clientSecretPath = os.path.join(currentFilePath, 'client_secret.json')
-    sheets = Sheets.from_files(clientSecretPath)
-    gsheetData = sheets[sheetId]
-    selectedDir = gsheetData[sheetTab][sheetCell]
-    newPath = os.path.join(serve_path, selectedDir)
-    return random_file_response(newPath, config)
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    client_secret_path = os.path.join(current_file_path, 'client_secret.json')
+    sheets = Sheets.from_files(client_secret_path)
+    gsheet_data = sheets[sheet_id]
+    selected_dir = gsheet_data[sheet_tab][sheet_cell]
+    new_path = os.path.join(serve_path, selected_dir)
+    return random_file_response(new_path, config)
     
